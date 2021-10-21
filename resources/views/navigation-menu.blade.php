44a72eb7fcba8490d8dab295d665566a76900b42
MysterRamen <karen.elemia@gmail.com>
2021-10-21 09:58:21 +0800

    v1

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -3,17 +3,19 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
+
+            
                 <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
+                <!-- <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-jet-application-mark class="block h-9 w-auto" />
                     </a>
-                </div>
+                </div> -->
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboardaaaaaaa') }}
+                        {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
 
@@ -21,7 +23,7 @@
                     <x-jet-nav-link href="{{ route('uploads.index') }}" :active="request()->routeIs('uploads.index')">
                         {{ __('Uploads') }}
                     </x-jet-nav-link>
-                </div>
+                </div> -->
 
                 
             </div>
