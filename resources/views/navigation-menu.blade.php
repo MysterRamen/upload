8ced271dc602d86df5793a842a90064174342ddb
MysterRamen <karen.elemia@gmail.com>
2021-10-21 15:23:18 +0800

    v8

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -24,11 +24,13 @@
                         {{ __('Uploads') }}
                     </x-jet-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+               
+                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('uploadstation.index') }}" :active="request()->routeIs('uploadstation.index')">
                         {{ __('Uploading Station') }}
                     </x-jet-nav-link>
                 </div>
+                
 
                 
             </div>
@@ -155,6 +157,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-jet-responsive-nav-link href="{{ route('uploads.index') }}" :active="request()->routeIs('uploads.index')">
+                {{ __('Uploads') }}
             </x-jet-responsive-nav-link>
         </div>
 
--- resources/views/uploadstation/index.blade.php
+++ resources/views/uploadstation/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Uploading Station') }}
+            {{ __('Create Lesson Material') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
     <form class="w-full max-w-lg" method="post" action="{{route('uploads.store')}}">
- 
+    @csrf
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="upload-title">
@@ -27,6 +27,39 @@
     </div>
   </div>
 
+  <div class="flex flex-wrap -mx-3 mb-2">
+    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="upload-assigned-lesson">
+        Assigned Lesson
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="upload-lecture-type">
+          <option value="{{ old('assignedLesson', 'Module 1') }}">Module 1</option>
+          <option value="{{ old('assignedLesson', 'Module 2') }}">Module 2</option>
+          <option value="{{ old('assignedLesson', 'Module 3') }}">Module 3</option>
+        </select>
+      </div>
+    </div>
+  
+  <div class="flex flex-wrap -mx-3 mb-2">
+    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="upload-lecture-type">
+        Lecture Type
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="upload-lecture-type">
+          <option value="{{ old('lessonType', 'Video Lecture') }}">Video Lecture</option>
+          <option value="{{ old('lessonType', 'Module Reading') }}">Module Reading</option>
+          <option value="{{ old('lessonType', 'Laboratory Guide') }}">Laboratory Guide</option>
+        </select>
+      </div>
+    </div>
+    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="upload-duration">
+        Duration
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="upload-duration" type="text" placeholder="10 minutes" value="{{ old('title', '') }}">
+    </div>
 
     <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="upload-file">
